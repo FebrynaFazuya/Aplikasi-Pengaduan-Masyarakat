80139c9b0604c2d314911710263a6e244075653c
Febrynafazuyaaaa <108813254+FebrynaFazuya@users.noreply.github.com>
2022-07-06 23:22:28 +0700

    Add files via upload

--- appem.sql
+++ appem.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Apr 2020 pada 21.32
--- Versi server: 10.1.36-MariaDB
--- Versi PHP: 7.2.10
+-- Generation Time: Jul 06, 2022 at 06:22 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `masyarakat`
+-- Table structure for table `masyarakat`
 --
 
 CREATE TABLE `masyarakat` (
@@ -37,17 +36,20 @@ CREATE TABLE `masyarakat` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `masyarakat`
+-- Dumping data for table `masyarakat`
 --
 
 INSERT INTO `masyarakat` (`nik`, `nama`, `ussername`, `password`, `telp`) VALUES
-('012345', 'Hitler', 'Hitler', '123', '08123456678'),
-('123', 'wati', 'wati', '123', '456475875');
+('084358389', 'Somi', 'somi', 'somi', '09745356334'),
+('45667', 'Wendy', 'wendy', 'wendy', '0878678564'),
+('56789', 'Kang Seulgi', 'seulgi', 'seulgi', '8972398469327'),
+('67834', 'Irene', 'irene', 'irene', '0987635423'),
+('97363', 'Yeri', 'yeri', 'yeri', '0868545245');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `pengaduan`
+-- Table structure for table `pengaduan`
 --
 
 CREATE TABLE `pengaduan` (
@@ -60,17 +62,24 @@ CREATE TABLE `pengaduan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `pengaduan`
+-- Dumping data for table `pengaduan`
 --
 
 INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nik`, `isi_laporan`, `foto`, `status`) VALUES
-(3, '2020-04-17', '012345', 'sdadfadf', 'Sakata.Gintoki.full.551386.jpg', '0'),
-(4, '2020-04-17', '012345', 'SDsd', '', '0');
+(14, '2020-04-20', '97363', 'saya melaporkan adanya pencurian', '', 'selesai'),
+(15, '2020-04-20', '97363', 'saya bertemu dengan penguasa yang semena mena', '380kZz.jpg', 'selesai'),
+(16, '2020-04-20', '56789', 'mencari orang hilang', 'unnamed.jpg', 'selesai'),
+(17, '2020-04-20', '56789', 'sampah menumpuk di jalan mohon untuk diperhatikan', 'sampah.jpg', 'selesai'),
+(18, '2020-04-20', '56789', 'mohon untuk membetulkan jalan', '97835194749-12-10_hl_--_jalan_rusak_ditanami_pohon_pisang.jpg', 'selesai'),
+(19, '2020-04-20', '084358389', 'mohon untuk diberi penerangan lagi atau satpam karena banyak begal', 'BERITA_1583406745_61b416a20981e5636e7dc8b2e6b71fad.jpg', '0'),
+(20, '2020-04-20', '084358389', 'terjadi pencurian ,mohon untuk lebih diperhatikan lagi keamanannya', 'robber-punches-police-escapes-in-hojai-39cd6fce2fde5bfbfbc6cd7ddf7d81d6_600x400.jpg', 'selesai'),
+(21, '2020-04-20', '084358389', 'masih banyak orang yang tidak menggunakan masker ,dimohon tuk sosialisasi dari wabah ini agar masyarakat lebih sadar', '', 'selesai'),
+(22, '2020-04-20', '45667', 'dimohon untuk pemotongan gaji pemerintah agar diberikan kepada rumah sakit untuk menangani virus corona', '', 'proses');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `petugas`
+-- Table structure for table `petugas`
 --
 
 CREATE TABLE `petugas` (
@@ -82,10 +91,18 @@ CREATE TABLE `petugas` (
   `level` enum('admin','petugas','','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `ussername`, `password`, `telp`, `level`) VALUES
+(1, 'Admin', 'admin', 'admin', '28347535', 'admin'),
+(5, 'Petugas', 'petugas', 'petugas', '09872354211', 'petugas');
+
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `tanggapan`
+-- Table structure for table `tanggapan`
 --
 
 CREATE TABLE `tanggapan` (
@@ -97,30 +114,43 @@ CREATE TABLE `tanggapan` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Dumping data for table `tanggapan`
+--
+
+INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `tanggapan`, `id_petugas`) VALUES
+(8, 14, '2020-04-20', '\r\ntapi keren', 1),
+(9, 15, '2020-04-20', '\r\n', 1),
+(10, 16, '2020-04-20', 'siap saya bantu carikan\r\n', 1),
+(11, 17, '2020-04-20', '\r\nsiap kami akan bantu', 1),
+(12, 18, '2020-04-20', '\r\nakan kami perbaiki', 1),
+(13, 20, '2020-04-20', '\r\nkami mengirimkan 2 pasukan CIA ', 1),
+(14, 21, '2022-02-01', '\r\nzdsdc', 1);
+
+--
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `masyarakat`
+-- Indexes for table `masyarakat`
 --
 ALTER TABLE `masyarakat`
   ADD PRIMARY KEY (`nik`);
 
 --
--- Indeks untuk tabel `pengaduan`
+-- Indexes for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
   ADD PRIMARY KEY (`id_pengaduan`),
   ADD KEY `nik` (`nik`);
 
 --
--- Indeks untuk tabel `petugas`
+-- Indexes for table `petugas`
 --
 ALTER TABLE `petugas`
   ADD PRIMARY KEY (`id_petugas`);
 
 --
--- Indeks untuk tabel `tanggapan`
+-- Indexes for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
   ADD PRIMARY KEY (`id_tanggapan`),
@@ -128,39 +158,39 @@ ALTER TABLE `tanggapan`
   ADD KEY `id_petugas` (`id_petugas`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `pengaduan`
+-- AUTO_INCREMENT for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
--- AUTO_INCREMENT untuk tabel `petugas`
+-- AUTO_INCREMENT for table `petugas`
 --
 ALTER TABLE `petugas`
-  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT untuk tabel `tanggapan`
+-- AUTO_INCREMENT for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
-  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `pengaduan`
+-- Constraints for table `pengaduan`
 --
 ALTER TABLE `pengaduan`
   ADD CONSTRAINT `pengaduan_ibfk_1` FOREIGN KEY (`nik`) REFERENCES `masyarakat` (`nik`);
 
 --
--- Ketidakleluasaan untuk tabel `tanggapan`
+-- Constraints for table `tanggapan`
 --
 ALTER TABLE `tanggapan`
   ADD CONSTRAINT `tanggapan_ibfk_1` FOREIGN KEY (`id_pengaduan`) REFERENCES `pengaduan` (`id_pengaduan`),
